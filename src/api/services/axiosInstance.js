5f03669fb66ba15f4a8b6d0286d240e38d920cbe
Tayyab Mazhar <tayyabrana213@gmail.com>
2024-03-23 05:28:13 +0500

    API Changed

--- src/api/services/axiosInstance.js
+++ src/api/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://helperzz-api.vercel.app/api',
+    baseURL: 'https://develons.site/api',
     headers: {
         'Content-Type': 'application/json',
     },
