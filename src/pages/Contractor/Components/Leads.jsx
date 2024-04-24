6adc18dc0d7b9b784dc86481e5d4eaf576246316
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-04-24 19:29:04 +0500

    reviews and leads

--- src/pages/Contractor/Components/Leads.jsx
+++ src/pages/Contractor/Components/Leads.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
-
+import React from "react";
+import moment from "moment";
 const Leads = ({ id, response }) => {
+  console.log(response);
+  return (
+    <>
+      {/* {id, user, subcategory, postal_code, home_type, time, details, status, created_at}) */}
+      <div>
+        <div className="flex flex-row flex-wrap gap-5 mt-6 ">
+          {response?.contractorDetails?.leads?.map((value) => (
+            <div className="flex flex-wrap w-[280px] min-h-[300px] text-center shadow-[rgba(0,0,15,0.05)_0px_0px_10px_5px] rounded-lg text-[#000000] text-sm py-5 justify-center px-4">
+              <div className="flex flex-col w-full">
+                <span className={`font-bold text-center mb-2`}>
+                  {value.user}
+                </span>
+                <span className={`font-bold text-left`}>
+                  {value.subcategory}
+                </span>
 
-    return (
-        <>
-            <div>
-                <div className="w-full flex flex-col justify-center">
-                    <div className={`grid grid-cols-2 gap-2`}>
-                        {response?.contractorDetails?.leads?.map((value) => (
-                            <div className="border-gray-600 border-[1px] rounded-lg text-[#000000] text-sm w-[100%]">
-                                <div
-                                    className="py-[2%] text-center text-md font-bold mx-auto  justify-center min-w-[50px]">
-                                    {value?.home_type}
-                                </div>
-                                <div
-                                    className="py-[2%] text-center text-lg font-semibold mx-auto  justify-center min-w-[50px]">
-                                    {value?.time}
-                                </div>
-                                <div
-                                    className="py-[2%] text-center text-sm font-medium mx-auto  justify-center min-w-[50px]">
-                                    {value?.details}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                <span className="flex text-left w-full justify-between align-center mt-2 ">
+                  <span className={`font-bold text-left`}>
+                    {value.postal_code}
+                  </span>
+                  <span className={`text-gray-500 text-sm`}>
+                    {moment(value.created_date).format("ll")}
+                  </span>
+                </span>
+
+                <div className="pt-[2%] text-left text-base font-semibold">
+                  {value?.home_type}
                 </div>
+                <div className="pt-[2%] text-left text-base font-semibold">
+                  {value?.time}
+                </div>
+                <div className="py-[2%] text-left text-sm">
+                  {value?.details}
+                </div>
+              </div>
             </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Leads;
--- src/pages/Contractor/Components/Reviews.jsx
+++ src/pages/Contractor/Components/Reviews.jsx
@@ -1,54 +1,80 @@
-import React from 'react';
-import {IMAGE_PATH} from "../../../utils/constants";
-import {Rating} from "@mui/material";
+import React from "react";
+import { IMAGE_PATH } from "../../../utils/constants";
+import { Rating } from "@mui/material";
+import moment from "moment";
 
-const Reviews = ({id, response}) => {
-    return (
-        <div>
-            <div className={`grid grid-cols-2 gap-2 mt-6`}>
-                {response?.contractorDetails?.reviews?.map((value) => (
-                    <div
-                        className={`flex flex-col text-center border-gray-600 border-[1px] rounded-2xl bg-gray-50`}
-                    >
-                        <div className={` flex flex-col py-6 justify-center items-center`}>
-                            <span className={`mt-2 font-bold`}>{value.title}</span>
-                            <Rating value={value.rating}/>
-                            <span className={`mt-1 font-medium text-[12px]`}>{value.review}</span>
-                            <div className={`grid grid-cols-5 gap-2 mt-2`}>
-                                {value.images.map((img) => (
-                                    <img
-                                        className="w-20 h-20 rounded-xl"
-                                        src={`${IMAGE_PATH}${img.image}`}
-                                        alt=""
-                                    />
-                                ))}
-                            </div>
-                            {value.status === 0 ?
-                                <div className={`mt-5 gap-2 text-white font-bold text-[12px]`}>
-                                    <button
-                                        className={`w-[100px] py-2 bg-green-700 rounded-lg mr-1 uppercase`}>Approve
-                                    </button>
-                                    <button className={`w-[100px] py-2 bg-red-700 rounded-lg ml-1 uppercase`}>Reject
-                                    </button>
-                                </div>
-                                :
-                                <>
-                                    {value.status === 1 ?
-                                        <button
-                                            className={`w-[200px] py-2 bg-green-700 rounded-lg mr-1 uppercase mt-5 text-[12px] font-bold text-white cursor-not-allowed`}>Approved</button>
-                                        :
-                                        <button
-                                            className={`w-[200px] py-2 bg-red-700 rounded-lg mr-1 uppercase mt-5 text-[12px] font-bold text-white cursor-not-allowed`}>Rejected</button>
-                                    }
-                                </>
-
-                            }
-                        </div>
-                    </div>
+const Reviews = ({ id, response }) => {
+  console.log(response);
+  return (
+    <div>
+      <div className={`flex flex-row flex-wrap gap-5 mt-6 `}>
+        {response?.contractorDetails?.reviews?.map((value) => (
+          <div
+            className={`flex flex-wrap w-[280px] text-center shadow-[rgba(0,0,15,0.05)_0px_0px_10px_5px] border-[1px] rounded-2xl bg-gray-50`}
+          >
+            <div className={` flex flex-col py-3 justify-center px-4`}>
+              <span className="flex text-left w-full justify-between align-center mt-2 ">
+                <span className={`font-bold text-left`}>{value.title}</span>
+                <span>
+                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAc0lEQVR4nNXTwQ2CQBAF0HdXaxDKMNgY2ohgC0ABWgxQiWaTPex9jOJP5voymfzh33LANc8+At3xytNFoGcBPSJQgwUzTraQGhNGVBFoKm40fgoaIlCVN0nI0RZyxporkKrw+0L2BXSLQOlRL2ixi0DfzxsZ/B2yf+xpkAAAAABJRU5ErkJggg==" />
+                </span>
+              </span>
+              <div className="text-left flex">
+                <Rating value={value.rating} size="small" readOnly />
+                <p className="text-gray-500 text-sm px-5">
+                  {moment(value.created_date).format("ll")}
+                </p>
+              </div>
+              <div className={`grid grid-cols-3 gap-2 mt-2`}>
+                {value.images.map((img) => (
+                  <img
+                    className="w-20 h-20 rounded-xl"
+                    src={`${IMAGE_PATH}${img.image}`}
+                    alt=""
+                  />
                 ))}
+              </div>{" "}
+              <span
+                className={`mt-2 min-h-[90px] text-left font-medium text-[12px]`}
+              >
+                {value.review}
+              </span>
+              {value.status === 0 ? (
+                <div className={`mt-5 gap-2 text-white font-bold text-[12px]`}>
+                  <button
+                    className={`w-[40%] py-2 bg-[#12947c] rounded-lg mr-1 uppercase`}
+                  >
+                    Approve
+                  </button>
+                  <button
+                    className={`w-[40%] py-2 bg-[#fd3d3a] rounded-lg ml-1 uppercase`}
+                  >
+                    Reject
+                  </button>
+                </div>
+              ) : (
+                <>
+                  {value.status === 1 ? (
+                    <button
+                      className={`w-[200px] py-2 bg-green-700 rounded-lg mr-1 uppercase mt-5 text-[12px] font-bold text-white cursor-not-allowed`}
+                    >
+                      Approved
+                    </button>
+                  ) : (
+                    <button
+                      className={`w-[200px] py-2 bg-red-700 rounded-lg mr-1 uppercase mt-5 text-[12px] font-bold text-white cursor-not-allowed`}
+                    >
+                      Rejected
+                    </button>
+                  )}
+                </>
+              )}
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
