b0a58717d18e7fe2959522ebe224cb14187867f4
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-04-28 00:28:56 +0500

    review

--- src/pages/Contractor/Components/Reviews.jsx
+++ src/pages/Contractor/Components/Reviews.jsx
@@ -4,20 +4,20 @@ import { Rating } from "@mui/material";
 import moment from "moment";
 import { Popover, Transition } from "@headlessui/react";
 import DeleteModal from "../../../components/DeleteModal";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import {
-    approveContractorReview,
-    deleteContractorReview,
-    rejectContractorReview
+  approveContractorReview,
+  deleteContractorReview,
+  rejectContractorReview,
 } from "../../../api/reducers/contractor";
-import {deleteReview} from "../../../api/reducers/review";
+import { deleteReview } from "../../../api/reducers/review";
 
 const Reviews = ({ id, response }) => {
   const [open, setOpen] = useState(false);
   const [deleteID, setDeleteID] = useState();
   const [openMenu, setOpenMenu] = useState();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const initiateDelete = (id) => {
     setOpen(!open);
@@ -25,7 +25,7 @@ const Reviews = ({ id, response }) => {
   };
 
   const handleDelete = () => {
-    dispatch(deleteContractorReview(deleteID))
+    dispatch(deleteContractorReview(deleteID));
   };
 
   return (
@@ -41,10 +41,10 @@ const Reviews = ({ id, response }) => {
           <div
             className={`flex flex-wrap w-[280px] min-h-[350px] text-center shadow-[rgba(0,0,15,0.05)_0px_0px_10px_5px] border-[1px] rounded-2xl bg-gray-50`}
           >
-            <div className={`relative flex flex-col py-3 justify-center px-4`}>
-              <span className="absolute top-0 left-0 right-0 px-4 pt-5  flex text-left w-full justify-between align-center mt-2 ">
+            <div className={`flex flex-col py-3 justify-center px-4`}>
+              <span className="flex text-left w-full justify-between align-center mt-2 ">
                 <span className={`font-bold text-left`}>{value.title}</span>
-                <Popover className="relative">
+                <Popover className="">
                   <Popover.Button onClick={() => setOpenMenu(!openMenu)}>
                     <span>
                       <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAc0lEQVR4nNXTwQ2CQBAF0HdXaxDKMNgY2ohgC0ABWgxQiWaTPex9jOJP5voymfzh33LANc8+At3xytNFoGcBPSJQgwUzTraQGhNGVBFoKm40fgoaIlCVN0nI0RZyxporkKrw+0L2BXSLQOlRL2ixi0DfzxsZ/B2yf+xpkAAAAABJRU5ErkJggg==" />
@@ -73,13 +73,13 @@ const Reviews = ({ id, response }) => {
                   </Transition>
                 </Popover>
               </span>
-              <div className="text-left flex absolute top-10 left-0 right-0 px-4 pt-5">
+              <div className="text-left flex pt-2">
                 <Rating value={value.rating} size="small" readOnly />
                 <p className="text-gray-500 text-sm px-5">
                   {moment(value.created_date).format("ll")}
                 </p>
               </div>
-              <div className={`grid grid-cols-3 gap-2 mt-2`}>
+              <div className={`grid grid-cols-3 gap-2 mt-2 h-[70px]`}>
                 {value.images.map((img) => (
                   <img
                     className="w-20 h-20 rounded-xl"
@@ -89,20 +89,22 @@ const Reviews = ({ id, response }) => {
                 ))}
               </div>{" "}
               <span
-                className={`mt-2 min-h-[90px] text-left font-medium text-[12px]`}
+                className={`mt-2 py-3 min-h-[100px] text-left font-medium text-[12px]`}
               >
                 {value.review}
               </span>
               {value.status === 0 ? (
-                <div onClick={() => dispatch(approveContractorReview(value.id))}
-                  className={`absolute bottom-4 px-4 mt-5 gap-2 text-white font-bold text-[12px]`}
+                <div
+                  onClick={() => dispatch(approveContractorReview(value.id))}
+                  className={`px-4 mt-5 gap-2 text-white font-bold text-[12px]`}
                 >
                   <button
                     className={`w-[100px] py-2 bg-[#12947c] rounded-lg mr-1 uppercase`}
                   >
                     Approve
                   </button>
-                  <button onClick={() => dispatch(rejectContractorReview(value.id))}
+                  <button
+                    onClick={() => dispatch(rejectContractorReview(value.id))}
                     className={`w-[100px] py-2 bg-[#fd3d3a] rounded-lg ml-1 uppercase`}
                   >
                     Reject
@@ -112,13 +114,13 @@ const Reviews = ({ id, response }) => {
                 <>
                   {value.status === 1 ? (
                     <button
-                      className={`absolute bottom-4 w-[100px] left-[30%]  py-2 bg-[#12947c] rounded-lg mr-1 uppercase text-[12px] font-bold text-white cursor-not-allowed`}
+                      className={` w-[100px] flex justify-center items-center py-2 bg-[#12947c] rounded-lg mr-1 uppercase text-[12px] font-bold text-white cursor-not-allowed`}
                     >
                       Approved
                     </button>
                   ) : (
                     <button
-                      className={`absolute bottom-4 w-[100px] left-[30%] py-2 bg-[#fd3d3a] rounded-lg ml-1 uppercase text-[12px] font-bold text-white cursor-not-allowed`}
+                      className={`w-[100px] flex justify-center items-center py-2 bg-[#fd3d3a] rounded-lg ml-1 uppercase text-[12px] font-bold text-white cursor-not-allowed`}
                     >
                       Rejected
                     </button>
